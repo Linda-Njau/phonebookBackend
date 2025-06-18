f47f2eb79be3d13d3c0d9304d44237ccb1d24109
Linda-Njau <lindanjau21@gmail.com>
2025-06-18 17:55:59 +0300

    Add functionality to create a new person with error checking

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+
 let persons = [
   {
     id: "1",
@@ -46,6 +48,29 @@ app.delete("/api/persons/:id", (request, response) => {
   const id = request.params.id;
   persons = persons.filter((person) => person.id !== id);
   response.status(204).end();
+});
+
+app.post("/api/persons", (request, response) => {
+  const body = request.body;
+  if (!body.name || !body.number) {
+    return response.status(400).json({
+      error: "must provide name and number",
+    });
+  }
+
+  const sameName = persons.find((person) => person.name === body.name);
+  if (sameName) {
+    return response.status(400).json({
+      error: "must provide unique name",
+    });
+  }
+
+  const person = {
+    ...body,
+    id: Math.floor(Math.random() * 1000000),
+  };
+  persons = persons.concat(person);
+  response.json(person);
 });
 
 const PORT = 3001;
