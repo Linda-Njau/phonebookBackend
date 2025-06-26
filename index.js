f432d807e8af73510ecb17bcc6e26421d0f48dc9
Linda-Njau <lindanjau21@gmail.com>
2025-06-26 15:16:14 +0300

    Add put method to update entry for person already in phonebook

--- index.js
+++ index.js
@@ -63,6 +63,23 @@ app.post("/api/persons", (request, response) => {
   });
 });
 
+app.put("/api/persons/:id", (request, response, next) => {
+  const { name, number } = request.body;
+  Person.findById(request.params.id)
+    .then((person) => {
+      if (!person) {
+        return response.status(404).end();
+      }
+      person.name = name;
+      person.number = number;
+
+      return person.save().then((updatedPerson) => {
+        response.json(updatedPerson);
+      });
+    })
+    .catch((error) => next(error));
+});
+
 const errorHandler = (error, request, response, next) => {
   if (error.name === "CastError") {
     return response.status(400).send({ error: "malformatted id" });
