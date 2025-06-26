742bd88747b61f40a7f33fc30c9c21f88458207f
Linda-Njau <lindanjau21@gmail.com>
2025-06-26 17:44:48 +0300

    Add custom validator to phonenumber check

--- models/person.js
+++ models/person.js
@@ -20,7 +20,15 @@ const personSchema = new mongoose.Schema({
     minLength: 3,
     required: true,
   },
-  number: String,
+  number: {
+    type: String,
+    validate: {
+      validator: function (v) {
+        return /^\d{2,3}-\d+$/.test(v);
+      },
+      message: (props) => `${props.value} is not a valid phone number.`,
+    },
+  },
 });
 
 personSchema.set("toJSON", {
